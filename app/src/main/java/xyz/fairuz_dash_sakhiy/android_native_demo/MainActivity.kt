75b808c69c6eca76679837376d3d9a82ab1027cd
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-03-02 22:18:09 +0700

    image card

--- app/src/main/java/xyz/fairuz_dash_sakhiy/android_native_demo/MainActivity.kt
+++ app/src/main/java/xyz/fairuz_dash_sakhiy/android_native_demo/MainActivity.kt
@@ -16,10 +16,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.layout.ContentScale
 import xyz.fairuz_dash_sakhiy.android_native_demo.ui.theme.AndroidnativedemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "Happy Birthday Fairuz!", from = "from Sakhiy", modifier = Modifier.padding(8.dp))
+                    GreetingImage(message = getString(R.string.happy_birthday_text), from = getString( R.string.signature_text))
                 }
             }
         }
@@ -56,15 +61,35 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F,
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     AndroidnativedemoTheme {
-        GreetingText(message = "Happy Birthday Fairuz", from = "From Sakhiy")
+        GreetingImage(message = "Happy Birthday Fairuz", from = "From Sakhiy", modifier = Modifier)
     }
 }
+
